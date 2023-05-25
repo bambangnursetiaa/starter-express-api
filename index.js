29ddea4cf04c2271ef797d42114b217654e80772
bambangnursetiaa <98002196+bambangnursetiaa@users.noreply.github.com>
2023-05-25 10:35:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post("/chat", async (req, res) => {
 
 async function sendMessageToChatGPT(message) {
   const apiUrl = "https://api.openai.com/v1/chat/completions";
-  const apiKey = "sk-sk-...pLW1"; // Ganti dengan API key Anda
+  const apiKey = "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"; // Ganti dengan API key Anda
   const model = "gpt-3.5-turbo"; // Model ChatGPT yang akan digunakan
 
   const response = await axios.post(
